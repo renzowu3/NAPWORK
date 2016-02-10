edd080b6e411c917f735cbac107aa25488472176
enzoviernes <enzo.viernes@gmail.com>
2016-02-10 18:34:38 +0800

    Applied exceptions

--- NAPWORK/src/napwork/SocketMethods.java
+++ NAPWORK/src/napwork/SocketMethods.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
@@ -11,6 +12,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class SocketMethods extends Device{
 	private int serverPortNum;
@@ -31,7 +33,7 @@ public class SocketMethods extends Device{
 	private DataOutputStream dos;
 	private int udpPortNum;
 	private DatagramSocket udpSoc;
-	private DatagramSocket udcServerSoc;
+	private DatagramSocket udpServerSoc;
 	private InetAddress receiverAddress;
 	private DatagramPacket receivePacket;
 	private DatagramPacket sendPacket;
